45d5eda40e12704b374452f4777faae6237cb97e
Andrey <andrew.gorba4ev2018@yandex.ru>
2022-12-07 22:53:04 +0300

    solution task1

---
+++ task1.js
@@ -0,0 +1,11 @@
+// сумма двух чисел
+const sum = (a, b) => console.log(a + b);
+sum(2, 3)
+
+// суммарное количество символов в обоихстроках
+const sumStrLen = (str1, str2) => sum(str1.length, str2.length);
+sumStrLen("qwerty", "test")
+
+// сумма цифр введённого числа
+const result = prompt('Введите трехзначное число');
+console.log("Сумма чисел:", result.split("").reduce((acc, el) => acc + +el, 0))
